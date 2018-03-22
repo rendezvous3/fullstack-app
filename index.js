d7ef0561aec1e0a170aa01106eaab8d66069d9c5
rendezvous3 <bojanj6m@gmail.com>
2018-03-22 02:47:46 -0400

    oauth leg 1

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const keys = require('./config/keys');
+
 const app = express();
 
 
@@ -12,7 +14,18 @@ const app = express();
 // -> Authorized redirect API's -> http://localhost:5000/* -> Create
 // -> Client id -> 362007267422-i4fll8npkt5pntuhad8i1kngtdkoelef.apps.googleusercontent.com
 // -> Client Secret -> ???
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken)
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email'],
+
+}));
 
 
 const PORT = process.env.PORT || 5000;
